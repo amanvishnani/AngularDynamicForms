c85111094e97ad5b0815dede34e9e3b62d8b67fc
amanvishnani <aman62@gmail.com>
2025-04-03 23:57:40 -0300

    Email using material

--- src/app/email-input/email-input.component.ts
+++ src/app/email-input/email-input.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-email-input',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    CommonModule, // Added for *ngIf
+    ReactiveFormsModule,
+    MatFormFieldModule, // Added for <mat-form-field>
+    MatInputModule, // Added for matInput
+  ],
   templateUrl: './email-input.component.html',
-  styleUrl: './email-input.component.css'
+  styleUrl: './email-input.component.css',
 })
 export class EmailInputComponent {
 
